87ce43aa8091cc2e045192d3fe101dede4990538
RickSchijvenaars <36455350+RickSchijvenaars@users.noreply.github.com>
2020-11-08 18:36:08 +0100

    Changed login to only name

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,31 +2,40 @@
 
 namespace App\Http\Controllers\Auth;
 
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
     use AuthenticatesUsers;
+
+    public function username()
+    {
+        return 'name';
+    }
+
+    protected function validateLogin(Request $request)
+    {
+        $this->validate($request, [
+            $this->username() => 'required|string',
+        ]);
+    }
+
+    protected function credentials(Request $request)
+    {
+        return array_merge($request->only($this->username()));
+    }
 
     /**
      * Where to redirect users after login.
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
