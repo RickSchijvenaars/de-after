1f6115bfe7a0499a5d2697462502dc5571c32d08
RickSchijvenaars <36455350+RickSchijvenaars@users.noreply.github.com>
2020-12-05 17:56:11 +0100

    Overrided UserProvider instead change in vendor

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        \Illuminate\Support\Facades\Auth::provider('customuserprovider', function($app, array $config) {
+            return new CustomUserProvider($app['hash'], $config['model']);
+        });
     }
 }
--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
+            'driver' => 'customuserprovider',
             'model' => App\User::class,
         ],
 
