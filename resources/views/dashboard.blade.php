6ea6ad314e865dfdc254ebe0eb17e3dc0ba0acb4
RickSchijvenaars <36455350+RickSchijvenaars@users.noreply.github.com>
2020-12-07 13:40:57 +0100

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,7 +16,7 @@
                     <div class="my-auto dashboard-livestream-iframe-container rounded-lg embed-responsive embed-responsive-16by9">
                         <iframe 
                             class="dashboard-livestream-iframe embed-responsive-item"
-                            src="https://embed.restream.io/player/index.htmltoken=75fa8?311f8a5180444940a0d7242e1ad" 
+                            src="https://www.youtube.com/embed/DHptIX78GdU" 
                             frameborder="0" 
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                             allowfullscreen
